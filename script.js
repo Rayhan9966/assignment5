50d547b77becea0df335f0d111e60ec26d32c645
Rayhan9966 <rayhancse162gmail.com>
2024-02-19 09:47:31 +0400

    js updated html updated

---
+++ script.js
@@ -0,0 +1,43 @@
+const bookedSeats = [ ];
+const totalSeat=8;
+const totalSeatElement=document.getElementById("seat-count");
+totalSeatElement.innerText=totalSeat;
+
+
+let ticketPriceElement=document.getElementById("ticket-price");
+
+
+function selectTicket(){
+
+  ticketPriceElement.innerText=550;
+
+}
+let ticketPriceElement=document.getElementById("ticket-price");
+
+
+function selectTicket(){
+
+  ticketPriceElement.innerText=550;
+
+}
+let ticketPriceElement=document.getElementById("ticket-price");
+
+
+function selectTicket(){
+
+  ticketPriceElement.innerText=550;
+
+}
+
+
+
+
+function button(){
+    const homePage=document.getElementById('hPage');
+    homePage.classList.add('hidden');
+    
+
+    const phPage=document.getElementById('ph-paribahon');
+    phPage.classList.remove('hidden');
+
+}
